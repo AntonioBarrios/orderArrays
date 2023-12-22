46507162176379a7c9082c8619cfaddbcb64dc9b
Antonio Arango <31933285+AntonioBarrios@users.noreply.github.com>
2023-12-22 11:13:48 -0600

    Update orderArrays.js

--- orderArrays.js
+++ orderArrays.js
@@ -1,7 +1,9 @@
 const inputArray= [10,6,2,15,11,7,21,8]
 const outputArray= [21,2,15,6,11,7,10,8]
 
-const newArray = inputArray.concat(outputArray)
+//const newArray = inputArray.concat(outputArray)
+//Using spread operator yo join arrays
+const newArray = [...inputArray, ...outputArray]
 
 const orderNumbers = newArray.reduce((acc, item) => {
     if(!acc.includes(item)){
